85c58b92e18d27f1a41006fe396ba4baa470562b
Luca <lightlucc@gmail.com>
2025-12-23 01:27:02 +0200

    workspace

---
+++ src/main/java/com/lucatm11/UsernameBlocker/Plugin.java
@@ -0,0 +1,16 @@
+package com.lucatm11.UsernameBlocker;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Plugin extends JavaPlugin
+{
+  public void onEnable()
+  {
+
+  }
+
+  public void onDisable()
+  {
+
+  }
+}
